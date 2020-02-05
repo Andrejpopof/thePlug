bcfe9f5e31940424d9227509c626033c45558101
Andrejpopof <andrejpopov.1430@gmail.com>
2020-02-06 00:14:48 +0100

    First Page finnished

--- app/src/main/java/com/example/theplug/Shop.java
+++ app/src/main/java/com/example/theplug/Shop.java
@@ -2,6 +2,7 @@ package com.example.theplug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Shop extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class Shop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        Intent intent=getIntent();
+        intent.getStringExtra("kluc");
+
     }
 }
